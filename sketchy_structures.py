89753337a0f985b53e33ea8b5a35dfeefe8166da
Jay Weeks <jay.patrick.weeks@gmail.com>
2017-06-30 12:20:47 -0700

    Add min and max distance params

--- sketchy_structures.py
+++ sketchy_structures.py
@@ -3,9 +3,37 @@ bl_info = {
     'category': 'Mesh',
 }
 
-import bpy, bmesh
+import bpy
+import bmesh
+from bpy.props import FloatProperty
 
-def create_edges(context):
+class SketchyStructuresOperator(bpy.types.Operator):
+    """Sketchy Structures"""
+    bl_idname = 'mesh.sketchy_structures_operator'
+    bl_label = 'Sketchy Structures'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    min_distance = FloatProperty(
+       name='Min Distance',
+       default=0.0,
+       subtype='DISTANCE',
+       unit='LENGTH')
+
+    max_distance = FloatProperty(
+       name='Max Distance',
+       default=100.0,
+       subtype='DISTANCE',
+       unit='LENGTH')
+
+    def execute(self, context):
+        res = create_edges(context,
+            min_distance=self.min_distance,
+            max_distance=self.max_distance)
+
+        self.report({'INFO'}, 'Created %s edges' % len(res['edges']))
+        return {'FINISHED'}
+
+def create_edges(context, min_distance, max_distance):
     edit_object = bpy.context.object
     edit_mesh = bmesh.from_edit_mesh(edit_object.data)
     selected_verts = [vert for vert in edit_mesh.verts if vert.select]
@@ -14,7 +42,12 @@ def create_edges(context):
     for i, vertA in enumerate(selected_verts):
         for vertB in selected_verts[i + 1:]:
             dist = (vertB.co - vertA.co).length
-            if dist > 0.0 and not edge_exists(edit_mesh, vertA, vertB):
+
+            if (vertA != vertB and
+                dist >= min_distance and
+                dist <= max_distance and not
+                edge_exists(edit_mesh, vertA, vertB)):
+
                 edge = add_edge(edit_mesh, vertA, vertB)
                 created_edges.append(edge)
 
@@ -29,17 +62,6 @@ def edge_exists(mesh, v0, v1):
 def add_edge(mesh, v0, v1):
     return mesh.edges.new((v0, v1))
 
-class SketchyStructuresOperator(bpy.types.Operator):
-    """Sketchy Structures"""
-    bl_idname = 'mesh.sketchy_structures_operator'
-    bl_label = 'Sketchy Structures'
-    bl_options = {'REGISTER', 'UNDO'}
-
-    def execute(self, context):
-        res = create_edges(context)
-        self.report({'INFO'}, 'Created %s edges' % len(res['edges']))
-        return {'FINISHED'}
-
 def draw_menu(self, context):
     self.layout.operator(SketchyStructuresOperator.bl_idname)
 
@@ -51,5 +73,5 @@ def unregister():
     bpy.utils.unregister_class(SketchyStructuresOperator)
     bpy.types.VIEW3D_MT_edit_mesh_edges.remove(draw_menu)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     register()
